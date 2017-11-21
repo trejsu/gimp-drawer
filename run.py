e8b598cba8d0806f547440e1354c391272b3fd4a
Martyna <trejsu@gmail.com>
2017-11-21 19:58:47 +0100

    Adds verbose parameter

--- run.py
+++ run.py
@@ -10,20 +10,26 @@ RUN_PARAMETER = "RUN-NONINTERACTIVE"
 def main(argv):
 
     infile = None
-    iterations = 0
+    corrections = 0
     acceptable_distance = 0
+    verbose = False
 
-    opts, args = getopt.getopt(argv, "f:i:d:")
+    opts, args = getopt.getopt(argv, "f:i:d:v")
     for opt, arg in opts:
         if opt == '-f':
             infile = arg
         if opt == '-i':
-            iterations = int(arg)
+            corrections = int(arg)
         if opt == '-d':
             acceptable_distance = arg
+        if opt == '-v':
+            verbose = True
 
-    os.system("gimp --verbose --debug-handlers --stack-trace-mode always -i -b '(python-fu-agent {} \"{}\" {} {})' -b '(gimp-quit 1)'"
-              .format(RUN_PARAMETER, infile, iterations, acceptable_distance))
+    command = "gimp {} -i -b '(python-fu-agent {} \"{}\" {} {})' -b '(gimp-quit 1)'"\
+        .format("--verbose --debug-handlers --stack-trace-mode always" if verbose else "",
+                RUN_PARAMETER, infile, corrections, acceptable_distance)
+
+    os.system(command)
 
 
 if __name__ == '__main__':
