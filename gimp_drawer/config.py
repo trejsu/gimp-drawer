ed3e1cc0e3eed9b6c02e7f71b3f368a747f099f7
Martyna <trejsu@gmail.com>
2017-12-23 19:27:46 +0100

    Changes config

--- gimp_drawer/config.py
+++ gimp_drawer/config.py
@@ -1,7 +1,7 @@
 improvements = dict(
     eps=0.05,
-    improvements_by_one_attempt=5,
-    attempts=30
+    improvements_by_one_attempt=20,
+    attempts=5
 )
 reducer_rate = 0.002
 timers = False
