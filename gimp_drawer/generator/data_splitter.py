0aa807b1d981201ed168f0dbc1fa38458575968d
Martyna <trejsu@gmail.com>
2018-01-10 17:30:48 +0100

    Adds validation of created data in data_splitter

--- gimp_drawer/generator/data_splitter.py
+++ gimp_drawer/generator/data_splitter.py
@@ -7,30 +7,39 @@ import random
 import tqdm
 
 import numpy as np
+from scipy import sum
 
 from gimp_drawer.generator.natural_keys import natural_keys
 
 
 def create_data(image_dirs, set_name, path):
-    def save(to_save, name): np.save("{}/{}_{}.npy".format(path, set_name, name), to_save)
-
     data = correlate_x_y_and_labels(image_dirs)
     random.shuffle(data)
 
-    X = []
-    Y = []
-    labels = []
+    X, Y, labels = initialize(len(data))
 
+    # todo: X, Y, labels should be appended to file
+    index = 0
     for d in tqdm.tqdm(data, "Creating {} data set".format(set_name)):
         x = np.load(d[0])
-        X.append(x.tolist())
+        X[index] = x
         y = np.load(d[1])
-        Y.append(y.tolist())
-        labels.append(d[2])
+        Y[index] = y
+        labels[index] = d[2]
+        index += 1
 
-    X = np.array(X)
-    Y = np.array(Y)
+    save_set(X, Y, labels, path, set_name)
 
+
+def initialize(size):
+    X = np.empty((size, 100, 100, 3))
+    Y = np.empty((size, 9))
+    labels = np.empty((size, 2), np.int)
+    return X, Y, labels
+
+
+def save_set(X, Y, labels, path, set_name):
+    def save(to_save, name): np.save("{}/{}_{}.npy".format(path, set_name, name), to_save)
     save(X, "X")
     save(Y, "Y")
     save(labels, "labels")
@@ -45,8 +54,38 @@ def correlate_x_y_and_labels(image_dirs):
         Y.sort(key=natural_keys)
         image_name = image.split("/")[-1]
         for x, y in zip(X, Y):
-            data.append((x, y, image_name))
+            action_number = x.split("/")[-1].split("_")[-1].split(".")[0]
+            data.append((x, y, (image_name, action_number)))
     return data
+
+
+def validate(X, Y, labels, path, set_name):
+    size = X.shape[0]
+    sample_indexes = random.sample(range(0, size - 1), 100)
+    for index in tqdm.tqdm(sample_indexes, "Validating {} data set".format(set_name)):
+        label = labels[index]
+        x_from_X = X[index]
+        image_name = label[0]
+        action_number = label[1]
+        x_from_file = np.load(path + "/{}/X_{}.npy".format(image_name, action_number))
+        assert sum(x_from_X - x_from_file) == 0
+        y_from_Y = Y[index]
+        y_from_file = np.load(path + "/{}/Y_{}.npy".format(image_name, action_number))
+        assert sum(y_from_Y - y_from_file) == 0
+
+
+def validate_data(path):
+    def load(name): return np.load(path + name, mmap_mode="r")
+
+    test_X = load("/test_X.npy")
+    train_X = load("/train_X.npy")
+    test_Y = load("/test_Y.npy")
+    train_Y = load("/train_Y.npy")
+    test_labels = load("/test_labels.npy")
+    train_labels = load("/train_labels.npy")
+
+    validate(train_X, train_Y, train_labels, path, "train")
+    validate(test_X, test_Y, test_labels, path, "test")
 
 
 def main(argv):
@@ -54,6 +93,7 @@ def main(argv):
     test_image_dirs, train_image_dirs = split_images(path, limit)
     create_data(train_image_dirs, "train", path)
     create_data(test_image_dirs, "test", path)
+    validate_data(path)
 
 
 def split_images(path, limit):
