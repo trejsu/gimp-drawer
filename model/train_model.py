5236fff98136c37f69a00b0c36d038a373129c84
Martyna <trejsu@gmail.com>
2018-02-12 19:48:49 +0100

    Parametrizes train_model.py

--- model/train_model.py
+++ model/train_model.py
@@ -1,27 +1,34 @@
 import sys
 import tqdm
-import getopt
+import argparse
 import os
+import json
 
 import numpy as np
 import tensorflow as tf
 
 
+ARGS = None
+FILTER_SIZE = 5
+CHANNELS = 3
+MODEL_DIR = os.path.expandvars("$GIMP_PROJECT/out/model/")
+
+
 def convolutional_network(image):
-    # First convolutional layer - maps image to 32 feature maps.
+    # First convolutional layer - maps image to ARGS.conv1 feature maps.
     with tf.name_scope('conv1'):
-        W_conv1 = weight_variable([5, 5, 3, 32])
-        b_conv1 = bias_variable([32])
+        W_conv1 = weight_variable([FILTER_SIZE, FILTER_SIZE, CHANNELS, ARGS.conv1])
+        b_conv1 = bias_variable([ARGS.conv1])
         h_conv1 = tf.nn.relu(conv2d(image, W_conv1) + b_conv1)
 
     # Pooling layer - downsamples by 2X.
     with tf.name_scope('pool1'):
         h_pool1 = max_pool_2x2(h_conv1)
 
-    # Second convolutional layer - maps 32 feature maps to 64.
+    # Second convolutional layer - maps ARGS.conv1 feature maps to ARGS.conv2.
     with tf.name_scope('conv2'):
-        W_conv2 = weight_variable([5, 5, 32, 64])
-        b_conv2 = bias_variable([64])
+        W_conv2 = weight_variable([FILTER_SIZE, FILTER_SIZE, ARGS.conv1, ARGS.conv2])
+        b_conv2 = bias_variable([ARGS.conv2])
         h_conv2 = tf.nn.relu(conv2d(h_pool1, W_conv2) + b_conv2)
 
     # Second pooling layer.
@@ -29,12 +36,12 @@ def convolutional_network(image):
         h_pool2 = max_pool_2x2(h_conv2)
 
     # Fully connected layer 1 - after 2 round of downsampling, our 100x100 image
-    # is down to 25x25x64 feature maps -- maps this to 512 features.
+    # is down to 25 x 25 x ARGS.conv2 feature maps -- maps this to ARGS.fc1 features.
     with tf.name_scope('fully_connected1'):
-        W_fully_conn1 = weight_variable([25 * 25 * 64, 512])
-        b_fully_conn1 = bias_variable([512])
+        W_fully_conn1 = weight_variable([25 * 25 * ARGS.conv2, ARGS.fc1])
+        b_fully_conn1 = bias_variable([ARGS.fc1])
 
-        h_pool2_flat = tf.reshape(h_pool2, [-1, 25 * 25 * 64])
+        h_pool2_flat = tf.reshape(h_pool2, [-1, 25 * 25 * ARGS.conv2])
         h_fully_conn1 = tf.nn.relu(tf.matmul(h_pool2_flat, W_fully_conn1) + b_fully_conn1)
 
     # Dropout - controls the complexity of the model, prevents co-adaptation of features.
@@ -42,9 +49,9 @@ def convolutional_network(image):
         keep_prob = tf.placeholder(tf.float32)
         h_fully_conn1_dropout = tf.nn.dropout(h_fully_conn1, keep_prob)
 
-    # Map the 512 features to 9 action arguments
+    # Map the ARGS.fc1 features to 9 action arguments
     with tf.name_scope('fully_connected2'):
-        W_fully_conn2 = weight_variable([512, 9])
+        W_fully_conn2 = weight_variable([ARGS.fc1, 9])
         b_fully_conn2 = bias_variable([9])
 
         y_conv = tf.matmul(h_fully_conn1_dropout, W_fully_conn2) + b_fully_conn2
@@ -61,30 +68,17 @@ def max_pool_2x2(x):
 
 
 def weight_variable(shape):
-    initial = tf.truncated_normal(shape, stddev=0.1)
+    initial = tf.truncated_normal(shape, stddev=ARGS.noise)
     return tf.Variable(initial)
 
 
 def bias_variable(shape):
-    initial = tf.constant(0.1, shape=shape)
+    initial = tf.constant(ARGS.init, shape=shape)
     return tf.Variable(initial)
 
 
-def read_train_set(data_dir):
-    X = np.load(data_dir + "/train_X.npy", mmap_mode="r")
-    Y = np.load(data_dir + "/train_Y.npy", mmap_mode="r")
-    return {
-        "X": X,
-        "Y": Y,
-    }
-
-
-def main(argv):
-
-    data_dir = "data/1125"
-    data = read_train_set(data_dir)
-    model_path = get_model_path(argv[1:])
-    saved_model_dir = os.path.expandvars("$GIMP_PROJECT/out/model/")
+def main(_):
+    data = read_train_set()
 
     x = tf.placeholder(tf.float32, [None, 100, 100, 3])
     y_ = tf.placeholder(tf.float32, [None, 9])
@@ -95,43 +89,68 @@ def main(argv):
         # loss = tf.reduce_mean(tf.square(tf.nn.sigmoid(y_conv) - y_))
 
     with tf.name_scope('optimizer'):
-        learning_rate = 0.0001
-        train_step = tf.train.AdamOptimizer(learning_rate).minimize(loss)
+        train_step = tf.train.AdamOptimizer(ARGS.rate).minimize(loss)
 
     error = tf.reduce_mean(loss)
 
     with tf.Session() as sess:
-
         saver = tf.train.Saver()
         sess.run(tf.global_variables_initializer())
-        step = 0 if model_path is None else int(model_path.split('-')[-1])
+        global_step = 0 if ARGS.model is None else int(ARGS.model.split('-')[-1])
+        if global_step != 0:
+            saver.restore(sess, ARGS.model)
 
-        if step != 0:
-            saver.restore(sess, model_path)
-
-        for i in tqdm.tqdm(range(step, 1759)):
-
-            batch_start = i * 50
-            X = data["X"][batch_start:batch_start + 50]
-            Y = data["Y"][batch_start:batch_start + 50]
-
-            if i % 100 == 0:
+        for step in tqdm.tqdm(range(global_step, 1759)):
+            X, Y = get_next_batch(data, step)
+            if step % 100 == 0:
                 train_error = error.eval(feed_dict={x: X, y_: Y, keep_prob: 1.0})
-                print('step %d, mean squared error %g' % (i, train_error))
-                if i != 0:
-                    saver.save(sess, saved_model_dir + "no_dropout", global_step=i)
+                tqdm.tqdm.write('step %d, mean squared error %g' % (step, train_error))
+                if step != 0:
+                    save_model(step, saver, sess)
+            train_step.run(feed_dict={x: X, y_: Y, keep_prob: ARGS.dropout})
 
-            train_step.run(feed_dict={x: X, y_: Y, keep_prob: 1.0})
+
+def save_model(step, saver, sess):
+    model_name = ARGS.name if ARGS.model is None else str(os.path.basename(ARGS.model))
+    path = MODEL_DIR + model_name
+    saver.save(sess, path, global_step=step)
+    config = {"noise": ARGS.noise, "init": ARGS.init, "conv1": ARGS.conv1, "conv2": ARGS.conv2,
+              "fc1": ARGS.fc1, "rate": ARGS.rate, "dropout": ARGS.dropout}
+    with open(path + "_config.json", 'w+') as outfile:
+        json.dump(config, outfile)
 
 
-def get_model_path(argv):
-    model_path = None
-    opts, args = getopt.getopt(argv, "m:")
-    for opt, arg in opts:
-        if opt == '-m':
-            model_path = arg
-    return model_path
+def get_next_batch(data, i):
+    batch_start = i * 50
+    X = data["X"][batch_start:batch_start + 50]
+    Y = data["Y"][batch_start:batch_start + 50]
+    return X, Y
+
+
+def read_train_set():
+    data_dir = "data/1125"
+    X = np.load(data_dir + "/train_X.npy", mmap_mode="r")
+    Y = np.load(data_dir + "/train_Y.npy", mmap_mode="r")
+    return {"X": X, "Y": Y}
 
 
 if __name__ == '__main__':
+    parser = argparse.ArgumentParser(formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+    parser.add_argument("-m", "--model", type=str,
+                        help="path to the partially trained model - when missing, the model will "
+                             "be trained from scratch")
+    parser.add_argument("--name", type=str, default="new_model",
+                        help="name of new model - ignored when --model argument provided")
+    parser.add_argument("--noise", type=float, default=0.1, help="weight variable stddev noise")
+    parser.add_argument("-i", "---init", default=0.1, type=float,
+                        help="bias variable initial value")
+    parser.add_argument("--conv1", default=32, type=int,
+                        help="number of filters in first convolutional layer")
+    parser.add_argument("--conv2", default=64, type=int,
+                        help="number of filters in second convolutional layer")
+    parser.add_argument("-f", "--fc1", default=512, type=int,
+                        help="number of neurons in first fully connected layer")
+    parser.add_argument("-r", "--rate", type=float, default=0.0001, help="learning rate")
+    parser.add_argument("-d", "--dropout", type=float, default=0.5, help="dropout")
+    ARGS = parser.parse_args()
     tf.app.run(main=main, argv=sys.argv)
