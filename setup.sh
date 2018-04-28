35edc54e1bcec2315fc1b72ff02fb6c123579d63
Martyna <trejsu@gmail.com>
2018-04-28 23:15:39 +0200

    Updates env vars

--- setup.sh
+++ setup.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 export SQUARE_MODEL_PATH="$GIMP_PROJECT/result/model/square"
-export SQUARE_CENTER_DATASET_PATH="$GIMP_PROJECT/data/dataset/square"
-export SQUARE_WITH_PARAMETERS_DATASET_PATH="$GIMP_PROJECT/data/dataset/square_with_parameters"
-export DIFF_SQUARE_WITH_PARAMETERS_DATASET_PATH="$GIMP_PROJECT/data/dataset/diff_square_with_parameters"
+export SQUARE_CENTER="$GIMP_PROJECT/data/dataset/square"
+export SQUARE_WITH_PARAMETERS="$GIMP_PROJECT/data/dataset/square_with_parameters"
+export DIFF_SQUARE_WITH_PARAMETERS="$GIMP_PROJECT/data/dataset/diff_square_with_parameters"
+export DIFF_RANDOM_SQUARE_WITH_PARAMETERS="$GIMP_PROJECT/data/dataset/diff_random_square_with_parameters"
