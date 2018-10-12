a03f3d45d6c0dc17faeb949ddbe0292084afd363
trejsu <trejsu@gmail.com>
2018-10-12 15:05:14 +0200

    Enhances plugin utilities
    * Adds possibility to pass parameters from dict
    * Adds running plugin as subprocess

--- src/common/plugin.py
+++ src/common/plugin.py
@@ -1,24 +1,36 @@
 import os
+from subprocess import Popen, PIPE
+
+REGISTER_ARG = '({}, "{}", "", {})'
+RUN_PLUGIN = "gimp -i -b '(python-fu-{} RUN-NONINTERACTIVE {})' -b '(gimp-quit 1)'"
+TYPE_TO_DEFAULT_VALUE = {'PF_STRING': "\"\"", 'PF_INT': 0, 'PF_FLOAT': 0, 'PF_BOOL': False}
+REGISTER = """register("{}", "", "", "", "", "", "", "", {}, [], plugin_main)\nmain()"""
 
 
 class Plugin(object):
-    DEFAULT = {'PF_STRING': "\"\"", 'PF_INT': 0, 'PF_FLOAT': 0, 'PF_BOOL': False}
 
-    def __init__(self, plugin_file, args, plugin_name):
+    def __init__(self, plugin_file, plugin_name, args, dict_args=False):
         self.plugin_file = plugin_file
         self.args = args
         self.plugin_name = plugin_name
+        self.dict_args = dict_args
 
     def run(self):
         self.create_or_update_plugin_file()
         self.run_plugin()
 
+    def run_as_subprocess(self):
+        self.create_or_update_plugin_file()
+        pid = self.run_plugin_as_subprocess()
+        return pid
+
     def create_or_update_plugin_file(self):
         with open(self.plugin_file) as f:
             plugin_body = f.read()
 
-        register_part = """register("{}", "", "", "", "", "", "", "", {}, [], plugin_main)\nmain()""".format(
-            self.plugin_name, self.parse_register_arguments()).replace("'", "")
+        register_part = REGISTER\
+            .format(self.plugin_name, self.parse_register_arguments())\
+            .replace("'", "")
 
         plugin = plugin_body + '\n' + register_part
 
@@ -28,17 +40,30 @@ class Plugin(object):
         os.system('chmod -R 700 "$GIMP_PLUGIN"')
 
     def run_plugin(self):
+        cmd = self.get_cmd()
+        os.system(cmd)
+
+    def run_plugin_as_subprocess(self):
+        cmd = self.get_cmd()
+        return Popen(cmd, shell=True, stdout=PIPE, stderr=PIPE)
+
+    def get_cmd(self):
         name = self.plugin_name.replace('_', '-')
-        command = "gimp -i -b '(python-fu-{} RUN-NONINTERACTIVE {})' -b '(gimp-quit 1)'".format(
-            name, self.parse_command_arguments())
-        os.system(command)
+        cmd = RUN_PLUGIN.format(name, self.parse_command_arguments())
+        return cmd
 
     def parse_register_arguments(self):
         arguments = []
-        for arg in vars(self.args):
-            gimp_type = self.get_type(getattr(self.args, arg))
-            arguments.append('({}, "{}", "", {})'.format(gimp_type, arg, Plugin.DEFAULT[gimp_type]))
+        for arg in self.arg_keys():
+            gimp_type = self.get_type(self.value(arg))
+            arguments.append(REGISTER_ARG.format(gimp_type, arg, TYPE_TO_DEFAULT_VALUE[gimp_type]))
         return arguments
+
+    def value(self, arg):
+        return self.args[arg] if self.dict_args else getattr(self.args, arg)
+
+    def arg_keys(self):
+        return self.args.keys() if self.dict_args else vars(self.args)
 
     @staticmethod
     def get_type(value):
@@ -55,8 +80,8 @@ class Plugin(object):
 
     def parse_command_arguments(self):
         arguments = ''
-        for arg in vars(self.args):
-            value = getattr(self.args, arg)
+        for arg in self.arg_keys():
+            value = self.value(arg)
             if isinstance(value, str):
                 arguments += '\"{}\" '.format(value)
             elif isinstance(value, bool):
